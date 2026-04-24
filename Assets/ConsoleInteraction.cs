0d5ebec9df11db0cd99c44a54e27a6baa599aadb
Jonathan Grinshpan <jonathan.grinshpan@gmail.com>
2026-04-24 17:10:06 +0300

    text machine buttom interactions

--- Assets/ConsoleInteraction.cs
+++ Assets/ConsoleInteraction.cs
@@ -1,30 +1,65 @@
-using UnityEngine;
+using System;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class MachineInteraction : MonoBehaviour
+public class ConsoleInteractable : MonoBehaviour
 {
-    public GameObject textUI; // drag MachineText here
+    [Header("Interaction")]
 
+    [Header("UI")]
+    public TextMeshProUGUI consoleText;         // drag your UI Text here
+    public GameObject textCanvas;    // drag your World Space Canvas here
+    public bool firstMessage = true;
+    public string startingMessage = "Press E to interact";
+
+    [Header("Messages")]
+    [TextArea]
+    public string[] messages = {
+        "SYSTEM BOOTING...",
+        "SYSTEM BOOTING....",
+                "SYSTEM BOOTING....",
+        "SYSTEM BOOTING......",        "SYSTEM BOOTING......",
+        "SYSTEM BOOTING.........",        "SYSTEM BOOTING...........",
+        "SYSTEM BOOTING..............."
+    };
+
+    public void Start()
+    {
+        consoleText.text = startingMessage;
+    }
+
+    private int currentIndex = -1;
     private bool playerInRange = false;
 
     void Update()
     {
+
+
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            ToggleText();
+            firstMessage = false;
+            currentIndex = (currentIndex + 1) % messages.Length;
+            consoleText.text = messages[currentIndex];
+
+
+            consoleText.lineSpacing -= 10;
+
+
         }
-    }
 
-    void ToggleText()
-    {
-        textUI.SetActive(!textUI.activeSelf);
+        // Optional: hide text when player walks away
+        if (!playerInRange && firstMessage)
+            textCanvas.SetActive(false);
     }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+
             playerInRange = true;
+            textCanvas.SetActive(true);
         }
     }
 
@@ -33,7 +68,13 @@ public class MachineInteraction : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            textUI.SetActive(false); // hide when leaving
+            currentIndex = -1;
+            if (firstMessage)
+            {
+                consoleText.text = startingMessage;
+            }
+
+
         }
     }
 }
